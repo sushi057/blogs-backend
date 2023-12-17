5f771e90cd0fbcccb054ce7527ca6edcd08374df
Suvash Joshi <suvash2077@gmail.com>
2023-12-17 23:49:19 +0545

    Added populate to schema

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, blogs: 1 });
   response.json(blogs);
 });
 
@@ -22,6 +23,7 @@ blogRouter.delete("/:id", async (request, response) => {
 
 blogRouter.post("/", async (request, response) => {
   const body = request.body;
+  const user = await User.findById(body.userId);
 
   if (!body.title) {
     return response.status(400).json({
@@ -33,10 +35,14 @@ blogRouter.post("/", async (request, response) => {
     title: body.title,
     url: body.url,
     author: body.author,
-    upvotes: body.upvotes,
+    upvotes: body.upvotes ? body.upvotes : 0,
+    user: user.id,
   });
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   response.status(201).json(savedBlog);
 });
 
